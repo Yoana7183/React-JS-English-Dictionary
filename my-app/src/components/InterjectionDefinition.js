b8979c97bc602b3710d81cd5b52547f130213b69
ynakova <yoana.nakova@yahoo.com>
2023-01-09 17:20:05 +0200

    strange random bug is fixed/ added more checks in Interjection definition module

--- my-app/src/components/InterjectionDefinition.js
+++ my-app/src/components/InterjectionDefinition.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InterjectionDefinition = ({ word }) => {
 
-  if (word.meanings[2] === undefined || word.meanings[2].definitions[1].definition === undefined || word.meanings[2].definitions[1] === undefined) {
+  if (word.meanings[2] === undefined || null || word.meanings[2].definitions[0].definition === undefined || null || word.meanings[2].definitions[1] === undefined || null) {
     return (
       <div className='noInterjection'>
         There is no Interjection definition for this word

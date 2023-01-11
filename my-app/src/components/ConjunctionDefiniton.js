613d33f96eabb81a9d92165980ed9f369c96a44c
y nakova <yoana.nakova@yahoo.com>
2023-01-11 16:16:58 +0200

    added new fileds / bug in part of speech part of components

---
+++ my-app/src/components/ConjunctionDefiniton.js
@@ -0,0 +1,24 @@
+
+
+const ConjunctionDefiniton = ({word}) => {
+  console.log(`word :${word}`);
+  if (word.meanings[2] === undefined) {
+    return(
+        <div className="noAdjective">There is no adjective definition for this word</div>
+        
+    )
+   
+}
+let definition = word.meanings[2].definitions[0].definition
+if (definition === undefined) {
+  definition = 'There is no adjective definition for this word'
+}
+return (
+  <div className="adjective">
+    Adjective definition:  {definition}
+  </div>
+)
+
+}
+
+export default ConjunctionDefiniton
